18a57475959a665c6f790c86b131e0ea68e464ac
CaianMorais <caianqm@gmail.com>
2025-01-07 11:22:13 -0400

    ajustes em contato por projeto e update do readme

--- README.md
+++ README.md
@@ -1,16 +1,23 @@
-<h2 align="center">Preview do projeto</h3>
-
-<h4>Página Principal</h4>
+<h3 align="center">Progresso do projeto<h3>
+<p align="center"><img src="https://progress-bar.xyz/57/?title=Conclu%C3%ADdo&width=300&progress_color=88f289"></p>
+<br>
+    
+<h3 align="center">Resumo</h3>
+<p>Abaixo temos um escopo do projeto, as prints é de uma build do projeto da data 06/01/2025.<br> Em breve, mais prints serão adicionadas conforme o andamento do projeto.</p>
+<br>
+    
+<h2 align="center">:camera:</h2>
+<h3>Página Principal</h3>
 <img src="https://github.com/user-attachments/assets/5d11a82f-3a35-479d-ad07-effb87f0bbd8">
 
-<h4>Menu de Projetos</h4>
+<h3>Menu de Projetos</h3>
 <img src="https://github.com/user-attachments/assets/5488860c-b9fe-4848-890a-802d13b752e7">
 
-<h4>Descrição do Projeto</h4>
+<h3>Descrição do Projeto</h3>
 <img src="https://github.com/user-attachments/assets/6e73b737-53af-4722-b725-5e50c4a4a0c0">
 
-<h4>Visão Geral Módulo de Administração</h4>
+<h3>Visão Geral Módulo de Administração</h3>
 <img src="https://github.com/user-attachments/assets/860c84f6-c180-42bc-8dcd-5d1a19fb56c1">
 
-<h4>Exemplo de Formulário de Edição no Módulo de Administração</h4>
+<h3>Exemplo de Formulário de Edição no Módulo de Administração</h3>
 <img src="https://github.com/user-attachments/assets/d94e4246-da18-450b-ad38-ffffcaef5074">
--- resources/views/admin/partials/contatos_por_projeto/_accordion_contatos.blade.php
+++ resources/views/admin/partials/contatos_por_projeto/_accordion_contatos.blade.php
@@ -1,6 +1,29 @@
 <div class="card-header" id="heading{{ $index }}" data-bs-toggle="collapse" data-bs-target="#collapse{{ $index }}"
     aria-expanded="false" aria-controls="collapse{{ $index }}" role="button">
-    <span class="collapse-title">
-        <strong>{{ $projeto->nome_projeto }}</strong> ({{ count($projeto->contatos) }} mensagens)
-    </span>
+    <div class="d-flex justify-content-between gap-3">
+
+        {{-- COLOCA O NOME DO PROJETO NO ACCORDION --}}
+        <span class="collapse-title flex-grow-1">
+            <strong>{{ $projeto->nome_projeto }}</strong>
+        </span>
+
+        {{-- COLOCA O STATUS NUM BADGE --}}
+        @if($projeto->status == 'AN')
+        <span class="badge bg-warning text-dark">Em andamento</span>
+        @elseif($projeto->status == 'PI')
+        <span class="badge bg-primary">Pronto para investimeto</span>
+        @elseif($projeto->status == 'CN')
+        <span class="badge bg-success">Concluído</span>
+        @endif
+
+        {{-- COLOCA O NÚMERO DE MENSAGENS NÃO LIDAS (SE HOUVER) NUM BADGE,
+         OU ENTAO AS LIDAS, SENAO SEM MENSAGENS --}}
+        @if(count($projeto->contatos) > 0 && count($projeto->contatos->where('lido', 0)) > 0)
+        <span class="badge bg-danger">{{ count($projeto->contatos->where('lido', 0)) }} não lidas</span>
+        @elseif(count($projeto->contatos) > 0 && count($projeto->contatos->where('lido', 1)) > 0)
+        <span class="badge bg-info text-dark">{{ count($projeto->contatos->where('lido', 1)) }} lidas</span>
+        @else
+        <span class="badge bg-secondary">Sem mensagens</span>
+        @endif
+    </div>
 </div>
